f528a630a6166946777271774d688c90d82d0d42
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2022-04-02 22:30:00 -0300

    update

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+
+import '../../styles/globals.css'
+import Head from 'next/head'
+function MyApp({ Component, pageProps }) {
+  return(
+    <>
+      <Head>
+        <title>Portifólio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  ) 
+    
+  
+}
+
+export default MyApp
